d24112d9427fcd3fcbeb9ee983b0cd5f50d496e0
HaiwangYu <yuhw.pku@gmail.com>
2019-11-13 23:37:10 -0500

    set cuda

---
+++ cpp/cuda-env.sh
@@ -0,0 +1,5 @@
+export             CUDADIR=/usr/local/cuda
+export                PATH=$CUDADIR/bin/:$PATH
+export     LD_LIBRARY_PATH=$CUDADIR/lib64:$LD_LIBRARY_PATH
+export   CUDA_INCLUDE_DIRS=$CUDADIR/include
+export CUDA_CUDART_LIBRARY=$CUDADIR/lib64
