c42c8a681e6ee6599f003080f2db1ea767488afb
HaiwangYu <yuhw.pku@gmail.com>
2019-12-12 16:26:42 -0500

    checkpoint: MP2

---
+++ predict.sh
@@ -0,0 +1,2 @@
+# ./predict_h5.py -m checkpoints/CP5-95.pth  --no-save --viz --no-crf --input test/data-0.h5 --mask-threshold 0.7 -e 9
+./predict_h5.py -m mp-tl3-th100/CP5-95.pth  --no-save --viz --no-crf --input test/data-0.h5 --mask-threshold 0.7 -e 0
---
+++ train.sh
@@ -0,0 +1,2 @@
+
+time ./train_h5.py -g -c mp-l23-th100/CP5-95.pth -e 5 -b 1
