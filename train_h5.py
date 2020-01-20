7cc7f58a5cf835aee260228745be2eba9510ef1f
HaiwangYu <yuhw.pku@gmail.com>
2020-01-20 17:59:45 -0500

    train 500 APA from epoch 5 - CP6

--- train_h5.py
+++ train_h5.py
@@ -19,7 +19,7 @@ def train_net(net,
               epochs=5,
               batch_size=10,
               lr=0.1,
-              val_percent=0.05,
+              val_percent=0.10,
               save_cp=True,
               gpu=False,
               img_scale=0.5):
@@ -28,10 +28,7 @@ def train_net(net,
     dir_mask = 'data/train_masks/'
     dir_checkpoint = 'checkpoints/'
 
-    file_img  = 'data/g4-rec-0.h5'
-    file_mask = 'data/g4-tru-0.h5'
-
-    ids = list(np.arange(100))
+    ids = list(np.arange(500))
 
     iddataset = split_train_val(ids, val_percent)
     print(iddataset['train'])
@@ -68,24 +65,34 @@ def train_net(net,
     truth_th = 100
 
     print('''
-    file_img: {}
-    file_mask: {}
     im_tags: {}
     ma_tags: {}
     truth_th: {}
-    '''.format(file_img, file_mask, im_tags,ma_tags,truth_th))
+    '''.format(im_tags,ma_tags,truth_th))
 
-    for epoch in range(epochs):
+    for epoch in range(5,epochs):
+
+        file_img  = 'data/cosmic-rec-0.h5'
+        file_mask = 'data/cosmic-tru-0.h5'
+        # if epoch % 2 != 0 :
+        #   file_img  = 'data/mu-rec-0.h5'
+        #   file_mask = 'data/mu-tru-0.h5'
+
+        print('''
+        file_img: {}
+        file_mask: {}
+        '''.format(file_img, file_mask))
+
         print('Starting epoch {}/{}.'.format(epoch + 1, epochs))
         net.train()
 
         train = zip(
           h5u.get_chw_imgs(file_img, iddataset['train'], im_tags, [1, 10], [800, 1600], [0, 600], 4000),
-          h5u.get_masks(file_mask, iddataset['train'], ma_tags, [1, 10], [800, 1600], [0, 600], truth_th)
+          h5u.get_masks(file_mask,   iddataset['train'], ma_tags, [1, 10], [800, 1600], [0, 600], truth_th)
         )
         val = zip(
-          h5u.get_chw_imgs(file_img, iddataset['val'], im_tags, [1, 10], [800, 1600], [0, 600], 4000),
-          h5u.get_masks(file_mask, iddataset['val'], ma_tags, [1, 10], [800, 1600], [0, 600], truth_th)
+          h5u.get_chw_imgs(file_img, iddataset['val'],   im_tags, [1, 10], [800, 1600], [0, 600], 4000),
+          h5u.get_masks(file_mask,   iddataset['val'],   ma_tags, [1, 10], [800, 1600], [0, 600], truth_th)
         )
 
         # for img, mask in train:
@@ -127,10 +134,10 @@ def train_net(net,
             loss.backward()
             optimizer.step()
 
-            if save_cp and i%20==0:
-                torch.save(net.state_dict(),
-                          dir_checkpoint + 'CP{}-{}.pth'.format(epoch + 1,i+1))
-                print('Checkpoint e{}b{} saved !'.format(epoch + 1,i+1))
+            # if save_cp and i%20==0:
+            #     torch.save(net.state_dict(),
+            #               dir_checkpoint + 'CP{}-{}.pth'.format(epoch + 1,i+1))
+            #     print('Checkpoint e{}b{} saved !'.format(epoch + 1,i+1))
 
         print('Epoch finished ! Loss: {}'.format(epoch_loss / i))
 
