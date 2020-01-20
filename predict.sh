e16671f81c2912960b8c8d2dd95f5b60f418641a
HaiwangYu <yuhw.pku@gmail.com>
2020-01-20 18:08:28 -0500

    update

--- predict.sh
+++ predict.sh
@@ -1,2 +1,4 @@
-# ./predict_h5.py -m checkpoints/CP5-95.pth  --no-save --viz --no-crf --input test/data-0.h5 --mask-threshold 0.7 -e 9
-./predict_h5.py -m mp-tl3-th100/CP5-95.pth  --no-save --viz --no-crf --input test/data-0.h5 --mask-threshold 0.7 -e 0
+# ./predict_h5.py -m backup/simchan-tune-tll-th100/CP5-95.pth  --viz --no-save --no-crf --input test/data-0.h5 --mask-threshold 0.7 -e 0
+
+./predict_h5.py -m l23-cosmic500-e50/CP9-450.pth --viz --no-save --no-crf --input test/data-0.h5 --mask-threshold -1 -e 0
+
--- train.sh
+++ train.sh
@@ -1,2 +1,3 @@
 
-time ./train_h5.py -g -c mp-l23-th100/CP5-95.pth -e 5 -b 1
+time ./train_h5.py -g --load mp-l23-IonAndScint-cosmic/CP5-450.pth -e 50 -b 1
+# time ./train_h5.py -g -e 5 -b 1
