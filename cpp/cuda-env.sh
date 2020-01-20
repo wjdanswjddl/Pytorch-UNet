ded5b337aa6ad4ceafdc08867b437db8dd170177
HaiwangYu <yuhw.pku@gmail.com>
2020-01-20 18:08:37 -0500

    update

--- cpp/cuda-env.sh
+++ cpp/cuda-env.sh
@@ -1,5 +1,13 @@
-export             CUDADIR=/usr/local/cuda
-export                PATH=$CUDADIR/bin/:$PATH
-export     LD_LIBRARY_PATH=$CUDADIR/lib64:$LD_LIBRARY_PATH
-export   CUDA_INCLUDE_DIRS=$CUDADIR/include
-export CUDA_CUDART_LIBRARY=$CUDADIR/lib64
+#export              CUDADIR=/usr/local/cuda
+#export                 PATH=$CUDADIR/bin/:$PATH
+#export      LD_LIBRARY_PATH=$CUDADIR/lib64:$LD_LIBRARY_PATH
+#export         LIBRARY_PATH=$CUDADIR/lib64:$LIBRARY_PATH
+#
+#export    CUDA_INCLUDE_DIRS=$CUDADIR/include
+#export  CUDA_CUDART_LIBRARY=$CUDADIR/lib64
+#
+#                 CUDNN_ROOT=$CUDADIR
+#export    CUDNN_INCLUDE_DIR=$CUDNN_ROOT/include
+#export        CUDNN_LIBRARY=$CUDNN_ROOT/lib64
+
+export      LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
