613fcde0a4210507d85c7d046804ec536a97d761
HaiwangYu <yuhw.pku@gmail.com>
2020-04-06 11:23:30 -0400

    update

--- compare_loss.py
+++ compare_loss.py
@@ -4,32 +4,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-
-
-
-# tags   = ['simchan-tune-ttl-th0/loss','simchan-tune-ttl-th50/loss','simchan-tune-ttl-th100/loss', 'mp-roi-test-1-scale-th100/loss', 'loss']
-# labels = ['TH: 0','TH: 50','TH: 100', 'MP3', 'MP2+MP3']
-tags   = ['l23-cosmic500-e50/loss', 'model-uresnet-512/loss']
-labels = ['UNet-1024', 'UResNet-512']
-dfs = [pd.read_csv(tag+'.csv', sep=' ', header=None) for tag in tags]
+tags   = ['model/unet-l23-cosmic500-e50', 'model/uresnet-512-l23-cosmic500-e50', 'model/nestedunet-l23-cosmic500-e50']
+labels = ['UNet', 'UResNet', 'NestedUNet']
+dfs = [pd.read_csv(tag+'/loss.csv', sep=' ', header=None) for tag in tags]
 
 epoch = 50
 nsample = 450
-
-# for i in range(len(tags)) :
-#   for isample in range(nsample*epoch):
-#     dfs[i][0][isample] = dfs[i][0][isample]+isample//nsample
-
-# for i in range(len(tags)) :
-#     # plt.plot(dfs[i][0][nsample*(epoch-1):nsample*(epoch)],dfs[i][1][nsample*(epoch-1):nsample*(epoch)],'o',label=labels[i])
-#     plt.plot(dfs[i][0],dfs[i][1],'o',label=labels[i])
-# plt.legend(loc='best',fontsize=15)
-# plt.grid()
-# # plt.ylim(0,800)
-# plt.xlabel("percentage", fontsize=15)
-# plt.ylabel("loss", fontsize=15)
-# plt.show()
-
 
 mean_loss = np.zeros((len(tags),epoch))
 
